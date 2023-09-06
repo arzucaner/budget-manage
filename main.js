d1aeaa9e562221085729ddc901a5f95ef2008381
Arzu Caner <arzuguneycaner@gmail.com>
2023-09-06 22:55:03 +0100

    Add initial Node.js code for budget manage
    This commit contains the initial Node.js code for Budget Manage, a budget management application.

---
+++ main.js
@@ -0,0 +1,66 @@
+class User {
+    constructor(username, password, email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    signUp() {
+
+    }
+
+    logIn() {
+
+    }
+}
+
+class FinancialRecord {
+    constructor(date, description, amount, category) {
+        this.date = date;
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+    }
+
+    save() {
+
+    }
+
+    update() {
+
+    }
+
+    delete() {
+
+    }
+}
+
+class Budget {
+    constructor(name, totalIncome, totalExpense, startDate, endDate) {
+        this.name = name;
+        this.totalIncome = totalIncome;
+        this.totalExpense = totalExpense;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    addIncome() {
+
+    }
+
+    addExpense() {
+
+    }
+
+    calculateTotalIncome() {
+
+    }
+
+    calculateTotalExpense() {
+
+    }
+
+    generateReport() {
+
+    }
+}
