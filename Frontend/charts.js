d80f91d26f6b29518d08ce7d52a11cc05247e1d5
Arzu Caner <arzuguneycaner@gmail.com>
2023-09-19 21:27:21 +0100

    Add enhanced chart with income data
    Updated the JavaScript code to include an additional dataset for displaying monthly income alongside monthly expenses. The chart now includes labels for the X and Y axes, and tooltips have been enabled for a better user experience. Also, the chart has a title for better context. These enhancements improve the overall presentation of budget data in the application.

--- Frontend/charts.js
+++ Frontend/charts.js
@@ -1,20 +1,54 @@
 var ctx = document.getElementById('myChart').getContext('2d');
+
 var myChart = new Chart(ctx, {
-    type: 'bar', 
+    type: 'bar',
     data: {
         labels: ['January', 'February', 'March', 'April', 'May'],
-        datasets: [{
-            label: 'Monthly Expenses',
-            data: [200, 300, 450, 250, 500], 
-            backgroundColor: 'rgba(75, 192, 192, 0.2)', 
-            borderColor: 'rgba(75, 192, 192, 1)', 
-            borderWidth: 1 
-        }]
+        datasets: [
+            {
+                label: 'Monthly Expenses',
+                data: [200, 300, 450, 250, 500],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            },
+            {
+                label: 'Monthly Income',
+                data: [500, 600, 550, 700, 800],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }
+        ]
     },
     options: {
         scales: {
             y: {
-                beginAtZero: true 
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Amount ($)'
+                }
+            },
+            x: {
+                title: {
+                    display: true,
+                    text: 'Month'
+                }
+            }
+        },
+        plugins: {
+            tooltip: {
+                enabled: true,
+                callbacks: {
+                    label: function (context) {
+                        return '$' + context.parsed.y;
+                    }
+                }
+            },
+            title: {
+                display: true,
+                text: 'Monthly Budget Overview'
             }
         }
     }
