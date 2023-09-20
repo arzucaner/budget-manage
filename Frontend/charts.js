3ad4e5d4256a96e8d19ffca3e158d46c70196795
Arzu Caner <arzuguneycaner@gmail.com>
2023-09-20 22:26:57 +0100

    Add multiple categories for expense tracking
    In this update,  expanded the budget tracking functionality to provide a more comprehensive view of your finances. Now, you can easily track expenses in various categories like Food, Housing, and Entertainment. This allows for a more detailed analysis of your spending habits and helps you make informed financial decisions.

--- Frontend/charts.js
+++ Frontend/charts.js
@@ -1,26 +1,35 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 
+var data = {
+    labels: ['January', 'February', 'March', 'April', 'May'],
+    datasets: [
+        {
+            label: 'Food',
+            data: [50, 70, 60, 80, 75],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Housing',
+            data: [300, 350, 320, 310, 330],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Entertainment',
+            data: [100, 120, 130, 110, 150],
+            backgroundColor: 'rgba(255, 205, 86, 0.2)',
+            borderColor: 'rgba(255, 205, 86, 1)',
+            borderWidth: 1
+        }
+    ]
+};
+
 var myChart = new Chart(ctx, {
     type: 'bar',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May'],
-        datasets: [
-            {
-                label: 'Monthly Expenses',
-                data: [200, 300, 450, 250, 500],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            },
-            {
-                label: 'Monthly Income',
-                data: [500, 600, 550, 700, 800],
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1
-            }
-        ]
-    },
+    data: data,
     options: {
         scales: {
             y: {
