a53e96bd6ce776f3dd1d46c5105d216f14630dfe
Arzu Caner <arzuguneycaner@gmail.com>
2023-09-26 23:17:23 +0100

    Update chart colors with a darker palette
    In this commit, I've updated the chart colors with a darker palette to align with the project's design theme of green, blue, and orange. The chart data sets for "Food," "Housing," and "Entertainment" have been adjusted to use deeper shades, providing a more visually appealing and professional look.
    
    - "Food" data set now uses a dark green background and border.
    - "Housing" data set now uses a dark blue background and border.
    - "Entertainment" data set now uses a dark orange background and border.
    
    This color update enhances the visual consistency of the project while ensuring that the charts remain easily readable. It aligns with the project's color scheme and design principles.

--- Frontend/charts.js
+++ Frontend/charts.js
@@ -15,22 +15,22 @@ var data = {
         {
             label: 'Food',
             data: [50, 70, 60, 80, 75],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(0, 100, 0, 0.4)',
+            borderColor: 'rgba(0, 100, 0, 1)',
             borderWidth: 1
         },
         {
             label: 'Housing',
             data: [300, 350, 320, 310, 330],
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(0, 0, 128, 0.4)',
+            borderColor: 'rgba(rgba(0, 0, 128, 1)',
             borderWidth: 1
         },
         {
             label: 'Entertainment',
             data: [100, 120, 130, 110, 150],
-            backgroundColor: 'rgba(255, 205, 86, 0.2)',
-            borderColor: 'rgba(255, 205, 86, 1)',
+            backgroundColor: 'rgba(255, 69, 0, 0.4)',
+            borderColor: 'rgba(255, 69, 0, 1)',
             borderWidth: 1
         }
     ]
